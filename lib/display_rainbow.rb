7767cefc78f450efcbff46ef7f9f4bd992820f23
Mendy <mendybeltcohen@gmail.com>
2018-05-23 15:36:37 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,5 @@
 # Write your #display_rainbow method here
 
 def display_rainbow(colors)
-  puts ["#{colors[0]}, #{colors[1]}, "]
+  puts ["#{colors[0]}, #{colors[1]}, #{colors[2]}, #{colors[3]}"]
+end
